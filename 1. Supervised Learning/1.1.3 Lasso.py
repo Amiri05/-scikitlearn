43b4fa945100a485f2259df597d2ad1685c8e5e7
Amiri05 <pranaysingh1503@gmail.com>
2026-04-15 14:37:37 +0100

    L1 L2

---
+++ 1. Supervised Learning/1.1.3 Lasso.py
@@ -0,0 +1,3 @@
+# This is a regression analysis method that performs both variable selection and regularization. 
+# It improves upon standard linear regression by adding a penalty proportional to the sum of the absolute values 
+# of the coefficients (regularization), which forces less significant feature coefficients to zero.
